7cfedf7b0588390bac08219f0a6d8dae0e3d3481
indriastuti-ichsan <148536071+indriastuti-ichsan@users.noreply.github.com>
2023-11-02 21:36:43 +0700

    Add files via upload

---
+++ estimasi_harga_pizza.py
@@ -0,0 +1,16 @@
+import pickle
+import streamlit as st
+
+model = pickle.load(open('estimasi_harga_pizza.sav','rb'))
+
+st.title('Estimasi Harga Pizza')
+diameter = st.number_input ("Masukkan Diameter Pizza", 0)
+n_topping = st.slider("Masukkan Jumlah Topping ",0, 2)
+
+predict = ''
+
+if st.button('Hitung Estimasi Harga'):
+    predict = model.predict(
+        [[diameter, n_topping]]
+    )
+    st.write ('Estimasi Harga Pizza = ',predict)
