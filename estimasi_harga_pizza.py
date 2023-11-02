45a2d39b845cf3d31adaf7845486e7c540c66271
indriastuti-ichsan <148536071+indriastuti-ichsan@users.noreply.github.com>
2023-11-02 21:57:41 +0700

    Update estimasi_harga_pizza.py

--- estimasi_harga_pizza.py
+++ estimasi_harga_pizza.py
@@ -5,8 +5,7 @@ model = pickle.load(open('estimasi_harga_pizza.sav','rb'))
 
 st.title('Estimasi Harga Pizza')
 diameter = st.number_input ("Masukkan Diameter Pizza", 0)
-n_topping = st.radio("Masukkan Jumlah Topping ",
-    ["0", "1", "2"])
+n_topping = st.slider("Masukkan Jumlah Topping ", 0, 100)
 
 predict = ''
 
